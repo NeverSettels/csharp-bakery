ae5dd07c6aecdfe12bf5fbe6dce6a5b41191cfcf
Student <example@example.com>
2020-02-28 11:15:52 -0800

    instanciate Bread's and Pastry's and add them to lists add empty show menu method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Collections.Generic;
 using Console = Colorful.Console;
 using Bakery.Models;
 using StyleSheet = Colorful.StyleSheet;
@@ -52,6 +53,17 @@ namespace Bakery
   '-,._   \__.-`-;''`          ``--'`'''''''``-- `--'--. '
   ";
   private static string _enter = @"Do you wish to enter the Bakery? [Y] yes [N] no";
+  private static Bread baguette = new Bread("baguette", 6);
+  private static Bread sourDoughRoll = new Bread("Roll", 4);
+  private static Bread normalRoll = new Bread("Roll", 2);
+  private static Pastry chocolateDonut = new Pastry("Donut", "Chocolate", 3);
+  private static Pastry glazedDonut = new Pastry("Donut", "Glazed", 3);
+  private static Pastry jellyDonut = new Pastry("Donut", "Jelly", 4);
+  private List<Bread> _breads = new List<Bread>{baguette, sourDoughRoll, normalRoll};
+  private List<Pastry> _pastrys = new List<Pastry>{chocolateDonut, glazedDonut, jellyDonut};
+
+          
+
         static void Main()
         {
        
@@ -108,11 +120,24 @@ namespace Bakery
         static void Shoping()
         {
           TypeLineFast(_bakeryName);
-          YesOrNo("Hi! Welcome to the Bakery! would you Like to Make a purchase? [Y] or [N]");
+          string purchase = YesOrNo("Hi! Welcome to the Bakery! Would you Like to Make a purchase? [Y] or [N]");
+          if(purchase == "y")
+          {
+            ShowMenu();
+          }
+          else{
+            GoodBye();
+          }
         }
         static void GoodBye()
         {
           
+        }
+        static void ShowMenu()
+        {
+         
+
+
         }
     }
 }
